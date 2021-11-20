f21f04be5bc477c0e40b8fe2f4eda38a06fed6d3
Laura Chan <chan@bryter.io>
2021-11-20 21:08:45 +0800

    Added test for article card

--- src/components/ArticleCard.test.ts
+++ src/components/ArticleCard.test.ts
@@ -1,6 +1,6 @@
 import { Article } from '../types'
 import ArticleCard from './ArticleCard.vue'
-import { render } from '@testing-library/vue'
+import { render, screen } from '@testing-library/vue'
 
 interface Props {
     article: Article
@@ -9,21 +9,30 @@ interface Props {
 function renderArticleCard(props: Props) {
     return render(ArticleCard, { props })
 }
-
 describe('ArticleCard', () => {
+    const article = {
+        by: 'someone',
+        descendants: 100,
+        id: 1049,
+        kids: [12, 123, 4, 324, 22, 3, 5],
+        score: 10,
+        time: Number(new Date(1234879999094)),
+        title: 'Title',
+        type: 'Some type',
+        url: 'http://www.article.com/',
+    }
+    beforeEach(() => {
+        renderArticleCard({
+            article
+        })
+    })
     it('displays the contents of the provided article', () => {
-        // renderArticleCard({
-        //     article: {
-        //         by: 'someone',
-        //         descendants: 100,
-        //         id: 1049,
-        //         kids: [12, 123, 4, 324, 22, 3, 5],
-        //         score: 10,
-        //         time: Number(Date.now()),
-        //         title: 'Title',
-        //         type: 'Some type',
-        //         url: 'http://www.article.com',
-        //     }
-        // })
+        screen.getByRole('heading', { name: article.title })
+        screen.getByText(`Score: ${article.score}`)
+        screen.getByText(`Comments: ${article.descendants}`)
+        screen.getByText(`${new Date(article.time * 1000)}`)
+    })
+    it('displays the article link', () => {
+        expect(screen.getByRole('link', { name: article.title })).toHaveProperty('href', article.url)
     })
 })
